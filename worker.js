bdf8917798695d078a242b731fdaa47f8641e184
AmarOdizo <amarkumarmunger2026@gmail.com>
2026-09-10 10:29:01 +0530

    change

--- worker.js
+++ worker.js
@@ -1,10 +1,10 @@
-const serverless = require('serverless-http');
-const app = require('./server');
+import serverless from 'serverless-http';
+import app from './server';
 
 // Convert Express app to Cloudflare Worker fetch handler
 const handler = serverless(app);
 
-module.exports = {
+export default {
   async fetch(request, env, ctx) {
     // Forward the request to the serverless-http handler
     return handler(request, env, ctx);
