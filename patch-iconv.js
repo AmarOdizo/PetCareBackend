4180c139b3c93a164b1d046e75bb0ff3f4177e16
AmarOdizo <amarkumarmunger2026@gmail.com>
2026-09-10 10:55:44 +0530

    cimmit

--- patch-iconv.js
+++ patch-iconv.js
@@ -1,14 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
-const file = path.join(__dirname, 'node_modules', 'iconv-lite', 'lib', 'index.js');
-if (fs.existsSync(file)) {
-    let content = fs.readFileSync(file, 'utf8');
-    // Comment out the problematic dynamic requires that esbuild resolves to empty objects
+const iconvFile = path.join(__dirname, 'node_modules', 'iconv-lite', 'lib', 'index.js');
+if (fs.existsSync(iconvFile)) {
+    let content = fs.readFileSync(iconvFile, 'utf8');
     content = content.replace('require("./streams")(iconv);', '/* require("./streams")(iconv); */');
     content = content.replace('require("./extend-node")(iconv);', '/* require("./extend-node")(iconv); */');
-    fs.writeFileSync(file, content);
+    fs.writeFileSync(iconvFile, content);
     console.log('Successfully patched iconv-lite for Cloudflare Workers.');
-} else {
-    console.log('iconv-lite not found, skipping patch.');
+}
+
+const mongoosePkg = path.join(__dirname, 'node_modules', 'mongoose', 'package.json');
+if (fs.existsSync(mongoosePkg)) {
+    let pkg = JSON.parse(fs.readFileSync(mongoosePkg, 'utf8'));
+    if (pkg.browser) {
+        delete pkg.browser;
+        fs.writeFileSync(mongoosePkg, JSON.stringify(pkg, null, 2));
+        console.log('Successfully patched mongoose for Cloudflare Workers (removed browser field).');
+    }
 }
